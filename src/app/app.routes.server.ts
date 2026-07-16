a4d2e191ab9530c688d29676ad18ed95679e429b
Sivasankar <info@pephands.com>
2026-07-16 15:21:41 +0530

    firebase activated

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'blogs/:blogUrl',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
